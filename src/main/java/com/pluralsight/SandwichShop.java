88cf4dc1bd1c5f1d29df2f1f6cf72c3126587724
nati-tewolde <natisnewemail@gmail.com>
2025-09-25 14:59:53 -0400

    refactored if statements

--- src/main/java/com/pluralsight/SandwichShop.java
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -13,20 +13,30 @@ public class SandwichShop {
                 "Type in 1 or 2: ");
         int orderSize = input.nextInt();
 
-        double basePrice = (orderSize == 1) ? 5.45 : 9.95;
+        // double basePrice = (orderSize == 1) ? 5.45 : 9.95;
+        double basePrice = 0;
+        if (orderSize == 1){
+            basePrice = 5.45;
+        } else if (orderSize == 2){
+            basePrice = 8.95;
+        } else {
+            System.out.println("Order size is not valid.");
+        }
 
         System.out.println("How old are you?");
         int age = input.nextInt();
 
-        double discountedPrice = basePrice;
+        double discount = 0;
         if (age <= 17) {
-            discountedPrice = basePrice - (basePrice * 0.1);
-        }
-        else if (age >= 65) {
-            discountedPrice = basePrice - (basePrice * 0.2);
+            discount = 0.1;
+        } else if (age >= 65) {
+            discount = 0.2;
         }
 
-        System.out.println("The cost of your sandwich is: " + discountedPrice);
+        double discountedPrice = basePrice - (basePrice * discount);
 
+        System.out.printf("The cost of your sandwich is: $%.2f", discountedPrice);
+
+        input.close();
     }
 }
