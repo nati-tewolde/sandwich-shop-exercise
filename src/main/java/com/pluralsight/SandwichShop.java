3c8d4c99ff19fca32eb51cf9ca43af3f2e04ea8a
nati-tewolde <natisnewemail@gmail.com>
2025-09-25 14:37:34 -0400

    completed step 1 of the exercise

--- src/main/java/com/pluralsight/SandwichShop.java
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -5,7 +5,28 @@ import java.util.Scanner;
 public class SandwichShop {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("What size sandwich would you like?\n" +
+                "1: Regular - base price - $5.45\n" +
+                "2: Large - base price - $8.95\n" +
+                "Type in 1 or 2: ");
+        int orderSize = input.nextInt();
+
+        double basePrice = (orderSize == 1) ? 5.45 : 9.95;
+
+        System.out.println("How old are you?");
+        int age = input.nextInt();
+
+        double discountedPrice = basePrice;
+        if (age <= 17) {
+            discountedPrice = basePrice - (basePrice * 0.1);
+        }
+        else if (age >= 65) {
+            discountedPrice = basePrice - (basePrice * 0.2);
+        }
+
+        System.out.println("The cost of your sandwich is: " + discountedPrice);
 
     }
 }
